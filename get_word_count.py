958023d82f1cecb9ccb3330716a3e1a0e066a0bd
Craig West <iwswordpress@gmail.com>
2024-05-04 14:52:03 +0100

    word count attempts

---
+++ get_word_count.py
@@ -0,0 +1,7 @@
+# iterate over all markdown files in the docs folder and sum the word count
+
+# use subprocess to execute the command line command mwc pytest/index.md
+# mwc .\docs\pytest\index.md
+import subprocess
+
+subprocess.run(["python", ".\README.md"])
